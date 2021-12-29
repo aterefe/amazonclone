4c7479ae49daef2ade26b8b1dc3581613e521bda
aterefe <aterefev2@gmail.com>
2021-12-29 16:46:03 -0600

    added the first product to the home page

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Product.css'
 
 function Product() {
   return (
-    <div>
-      
+    <div className='product'>
+      <div className='product_info'>
+        <p>The lean startup</p>
+        <p className='product_price'>
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className='product_rating'>
+          <p>⭐️</p>
+        </div>
+      </div>
+      <img 
+      src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+      alt=''
+      />
+      <button>Add to Basket</button>
     </div>
   )
 }
