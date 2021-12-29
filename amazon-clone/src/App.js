2a4a1015bab2fdb3a12f6827b163796045005ae4
aterefe <aterefev2@gmail.com>
2021-12-28 22:33:12 -0600

    created a home component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
+import Home from './Home'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     //Bem convention
     <div className="app">
         <Header />
-        {/* Header (products) */}
+        <Home />
     </div>
   );
 }
