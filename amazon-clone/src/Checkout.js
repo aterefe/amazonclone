d536cc732a8d5e3563d24660debecb09c4b60932
aterefe <aterefev2@gmail.com>
2022-01-05 20:59:08 -0600

    added and styled the shopping basket & subtotal

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -10,6 +10,14 @@ export default function Checkout() {
           src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
           alt=''
         />
+
+        <div className='checkout_title'>
+          <h2>Your Shopping basket</h2>
+        </div>       
+      </div>
+
+      <div className='checkout_right'>
+        <h2>Subtotal will go here!</h2>
       </div>
     </div>
   )
