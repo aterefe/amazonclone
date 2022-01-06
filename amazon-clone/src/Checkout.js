10e54e59cc1b4daada3fd39e6d302fcd21d9c243
aterefe <aterefev2@gmail.com>
2022-01-05 20:45:41 -0600

    added an ad to the checkout page

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -4,7 +4,13 @@ import './Checkout.css'
 export default function Checkout() {
   return (
     <div className='checkout'>
-      <h1>Let's GOOOOOOOO!</h1>
+      <div className='checkout_left'>
+        <img
+          className='checkout_ad'
+          src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+          alt=''
+        />
+      </div>
     </div>
   )
 }
