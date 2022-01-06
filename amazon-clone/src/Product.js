55e27ccc1bba6c225be551490405067d771df820
aterefe <aterefev2@gmail.com>
2022-01-05 19:34:35 -0600

    Made the product dynmaic allowing paramaters

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -5,17 +5,21 @@ function Product({title, image, price, rating}) {
   return (
     <div className='product'>
       <div className='product_info'>
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className='product_price'>
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className='product_rating'>
-          <p>⭐️</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
       <img 
-      src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+      src={image}
       alt=''
       />
       <button>Add to Basket</button>
